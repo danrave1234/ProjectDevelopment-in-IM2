ac01029bbb57b61b001ed6ec241369289ed859d8
Ehrica Jynne Espada <ehricajynne16@gmail.com>
2024-11-01 23:48:58 +0800

    user login and register w/ logout

--- user/views.py
+++ user/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
-def user_test(request):
-    return HttpResponse("<h1>Hello, this is the views for user</h1>")
+# Register
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+# Login
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')  
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+# Logout
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def dashboard(request):
+    user = request.user  
+    return render(request, 'inventory/dashboard.html', {'user': user})
