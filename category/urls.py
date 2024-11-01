15e3dafb03b8673330066c1a8ba34639a92a2544
K1kks <mikaellorenzocuyugan@gmail.com>
2024-11-01 22:57:13 +0800

    Category done

--- category/urls.py
+++ category/urls.py
@@ -1,8 +1,9 @@
-
 from django.urls import path
-
-from category.views import category_test
+from . import views
 
 urlpatterns = [
-    path('test/', category_test, name='category'),
+    path('test/', views.categories_test, name='test'),  # Test view for categories
+    path('register/', views.register_category, name='register_category'),  # View to register a new category
+    path('search/', views.search_categories, name='search_category'),  # View to search for categories
+    path('manage/', views.manage_categories, name='manage_categories'),  # View to manage categories
 ]
